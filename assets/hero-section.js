4423ac35dca6f10b5e70161a47b97aa6457ebae4
kyle <andeskyle22@gmail.com>
2023-10-27 20:13:20 +0800

    #

--- assets/hero-section.js
+++ assets/hero-section.js
@@ -6,7 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
     'https://cdn.shopify.com/s/files/1/0832/6139/1134/files/different.png?v=1698407623',
   ];
 
-  const backgroundElement = document.querySelector('.background'); // Update the selector with the correct class or ID
+  let newBottleImageUrl = [
+    "{{'Fresh Milk Bottle.png' | asset_url}}",
+    "{{'FRONT_STRAW.png' | asset_url}}",
+    "{{'FRONT_CHOC.png' | asset_url}}",
+    "{{'FRONT_PAN.png' | asset_url}}",
+  ];
+  let newButtonImageUrl = [
+    "{{'FM_button.png' | asset_url}}",
+    "{{'ST_button.png' | asset_url}}",
+    "{{'CH_button.png' | asset_url}}",
+    "{{'PA_button.png' | asset_url}}",
+  ];
+
+  const backgroundElement = document.querySelector('.background');
+  const bottleElement = document.querySelector('.bottle');
+  const buttonElement = document.querySelector('.fm-button'); // Select the button element by its class
 
   let timer;
   let continueTimer = true;
@@ -17,9 +32,16 @@ document.addEventListener('DOMContentLoaded', () => {
       return; // Stop the timer if continueTimer is false
     }
 
+    // Change background image
     backgroundElement.style.backgroundImage = `url('${newBackgroundImageUrl[currentIndex]}')`;
 
-    // Increment currentIndex for the next image, cycling back to 0 if it exceeds array length
+    // Change bottle image
+    bottleElement.src = newBottleImageUrl[currentIndex];
+
+    // Change button image
+    buttonElement.src = newButtonImageUrl[currentIndex];
+
+    // Increment currentIndex for the next images, cycling back to 0 if it exceeds array length
     currentIndex = (currentIndex + 1) % newBackgroundImageUrl.length;
 
     // Wait for the animation to finish and then reset
@@ -30,6 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 500); // Adjust the delay to match the animation duration
   }
 
-  // Call the function to start the background image changing process
+  // Call the function to start the background, bottle, and button image changing process
   imgAndBackgroundChanger();
 });
